c91810def3835c793a9df34f8f04be9596bd9011
Chinguunee <u.chingunn@gmail.com>
2026-01-09 09:05:55 +0800

    minor

--- src/app/tour/page.jsx
+++ src/app/tour/page.jsx
@@ -67,11 +67,10 @@ export default function Home() {
   );
 
   const toggleDesc = (id) => {
-    setTours((prevTours) =>
-      prevTours.map((tour) =>
-        tour.id === id ? { ...tour, showMore: !tour.showMore } : tour
-      )
+    const updatedTours = tours.map((tour) =>
+      tour.id === id ? { ...tour, showMore: !tour.showMore } : tour
     );
+    setTours(updatedTours);
   };
 
   const clearAll = () => {
@@ -94,40 +93,52 @@ export default function Home() {
       <div className="grid grid-cols-3 gap-8 items-start">
         {tours.map((tour) => {
           return (
-            <div
-              className="flex flex-col bg-white w-75 h-auto rounded-2xl justify-start items-center gap-2 shadow-[2px_-1px_6px_2px_rgba(0,_0,_0,_0.1)] mb-5 overflow-hidden transition-[max-height] duration-300 ease-in-out"
+            <Tour
               key={tour.id}
-            >
-              <p className="flex absolute ml-55 bg-green-500 w-20 items-center justify-center rounded-tr-xl rounded-bl-xl font-bold text-white">
-                {tour.price}
-              </p>
-              <img
-                className="w-75 h-50 object-cover rounded-tl-2xl rounded-tr-2xl"
-                src={tour.img}
-                alt=""
-              />
-              <p className="text-black my-2 mx-15 h-auto text-center font-bold">
-                {tour.name}
-              </p>
-              <p className="text-gray-500 mx-8">
-                {tour.showMore ? tour.longDesc : tour.shortDesc}
-              </p>
-              <button
-                className="text-green-500 transition-colors duration-200 ease-in-out hover:text-green-800"
-                onClick={() => toggleDesc(tour.id)}
-              >
-                Read More
-              </button>
-              <button
-                className="text-green-600 border-1 w-60 mb-5 rounded-2xl transition-colors duration-200 ease-in-out hover:bg-green-600 hover:text-white"
-                onClick={() => removeTour(tour.id)}
-              >
-                Not Interested
-              </button>
-            </div>
+              tour={tour}
+              toggleDesc={toggleDesc}
+              removeTour={removeTour}
+            />
           );
         })}
       </div>
     </div>
   );
 }
+
+const Tour = ({ tour, toggleDesc, removeTour }) => {
+  const [isShow, setIsShow] = useState(false);
+  return (
+    <div
+      className="flex flex-col bg-white w-75 h-auto rounded-2xl justify-start items-center gap-2 shadow-[2px_-1px_6px_2px_rgba(0,_0,_0,_0.1)] mb-5 overflow-hidden transition-[max-height] duration-300 ease-in-out"
+      key={tour.id}
+    >
+      <p className="flex absolute ml-55 bg-green-500 w-20 items-center justify-center rounded-tr-xl rounded-bl-xl font-bold text-white">
+        {tour.price}
+      </p>
+      <img
+        className="w-75 h-50 object-cover rounded-tl-2xl rounded-tr-2xl"
+        src={tour.img}
+        alt=""
+      />
+      <p className="text-black my-2 mx-15 h-auto text-center font-bold">
+        {tour.name}
+      </p>
+      <p className="text-gray-500 mx-8">
+        {tour.showMore ? tour.longDesc : tour.shortDesc}
+      </p>
+      <button
+        className="text-green-500 transition-colors duration-200 ease-in-out hover:text-green-800"
+        onClick={() => toggleDesc(tour.id)}
+      >
+        Read More
+      </button>
+      <button
+        className="text-green-600 border-1 w-60 mb-5 rounded-2xl transition-colors duration-200 ease-in-out hover:bg-green-600 hover:text-white"
+        onClick={() => removeTour(tour.id)}
+      >
+        Not Interested
+      </button>
+    </div>
+  );
+};
